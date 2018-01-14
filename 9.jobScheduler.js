e036b694f55dcf5ad3eeac20ce90c7238c76b3f0
juliazxu <juliazxu@gmail.com>
2018-01-13 20:13:33 -0800

    Added 2 more problems

---
+++ 9.jobScheduler.js
@@ -0,0 +1,16 @@
+// This problem was asked by Apple.
+
+// Implement a job scheduler which takes in a function f and an integer n, 
+// and calls f after n milliseconds.
+
+let f = () => {
+  alert('Schedule a job!');
+}
+
+let n = 100;
+
+const jobScheduler = (f, n) => {
+  this.setTimeout(f, n);
+}
+
+jobScheduler(f, n);
