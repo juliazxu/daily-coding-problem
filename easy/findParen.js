b57a82d8876f54529aab6749a76a2cf19818e3fb
juliazxu <juliazxu@gmail.com>
2018-05-11 23:04:39 -0700

    Optimized, added edge cases and fixed bugs

--- easy/findParen.js
+++ easy/findParen.js
@@ -9,22 +9,57 @@
 // (idx of the first parenthesis), the output should be 79 
 // (idx of the last parenthesis).
 
+// Edge cases
+
+// test for invalid input
+// do early return if you already found paren
+// account for when you can't find end paren
+
 const findParen = (str, firstParenIndex) => {
+  if (str[firstParenIndex] !== '(') {
+    return 'Input start paren index is not valid.';
+  }
   let openingParens = {};
   let closingParens = {};
   let idx = 0;
   let identifier = 1;
+  let lastItem = null;
+  let highestIdentifier = 0;
   for (let char of str) {
     if (char === '(') {
+      if (identifier <= highestIdentifier) {
+        identifier = highestIdentifier + 1;
+      }
+      // console.log('char is', char, 'identifier is', identifier, 'idx is', idx, 'highestIdentifier', highestIdentifier)
       openingParens[idx] = identifier;
-      identifier++;
+      lastItem = 'opening';
+      if (identifier > highestIdentifier) {
+        highestIdentifier = identifier;
+      }
     } else if (char === ')') {
-      identifier--;
+      if (lastItem === 'closing') {
+        identifier--;
+      }
+      while (closingParens[identifier]) {
+        identifier--;
+      }
+      // console.log('char is', char, 'identifier is', identifier, 'idx is', idx, 'highestIdentifier', highestIdentifier)
       closingParens[identifier] = idx;
+      lastItem = 'closing';
     }
     idx++;
+    if (closingParens[openingParens[firstParenIndex]]) {
+      return closingParens[openingParens[firstParenIndex]];
+    }
   }
-  return closingParens[openingParens[firstParenIndex]];
+  // console.log(openingParens, closingParens);
+  return 'No end paren found.';
 }
 
+findParen('()()', 0); // 1
+findParen('()()', 2); // 3
+findParen('()(', 2); // No end paren found.
+findParen('()(', 1); // Input start paren index is not valid.
+findParen('(he(lll)o)()?', 0); // 9
+findParen('(he(lll)o)()?', 3); // 7
 findParen("Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.", 10)
